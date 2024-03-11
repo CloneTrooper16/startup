ff7f205a43fd3497a4cf5b86660d58ebbcf1b7dd
Spencer Winward <srwinz@gmail.com>
2024-03-11 15:55:37 -0600

    Added quote to about page

--- public/about.js
+++ public/about.js
@@ -1,19 +1,29 @@
+
 function displayQuote() {
-    fetch('https://api.adviceslip.com/advice')
+    let bannedIDs = [24,27,28,29,31,33,34,37,38,46,75,76,80,114,181,183,203];
+    let brokenIDs = [22,48,67,225];
+    fetch(`https://api.adviceslip.com/advice`)
       .then((response) => response.json())
       .then((data) => {
-        const containerEl = document.querySelector('#quote');
-        console.log(data);
-        const adviceEl = document.createElement('p');
-        adviceEl.classList.add('quote');
-        // const authorEl = document.createElement('p');
-        // authorEl.classList.add('author');
-  
-        adviceEl.textContent = data.slip.advice;
-        // authorEl.textContent = data.that;
-  
-        containerEl.appendChild(adviceEl);
-        // containerEl.appendChild(authorEl);
+        // console.log(data.slip.id);
+        if (bannedIDs.find(id => id == data.slip.id)) {
+            // console.log("banned!");
+            displayQuote();
+        } else {
+            const containerEl = document.querySelector('#quote');
+            console.log(data.slip);
+            const adviceEl = document.createElement('p');
+            adviceEl.classList.add('quote');
+            // const authorEl = document.createElement('p');
+            // authorEl.classList.add('author');
+      
+            adviceEl.textContent = data.slip.advice;
+            // authorEl.textContent = data.that;
+      
+            containerEl.appendChild(adviceEl);
+            // containerEl.appendChild(authorEl);
+        }
+
       });
   }
 
