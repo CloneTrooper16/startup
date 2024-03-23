3f92c45e230bb7d6456496ebcf41d0cb44d15586
Spencer Winward <srwinz@gmail.com>
2024-03-22 21:44:49 -0600

    changed collection name

--- database.js
+++ database.js
@@ -7,7 +7,7 @@ const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostna
 const client = new MongoClient(url);
 const db = client.db('startup');
 const userCollection = db.collection('user');
-const scoreCollection = db.collection('score');
+const scoreCollection = db.collection('scores');
 
 //test database
 (async function testConnection() {
